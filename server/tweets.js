e47c1c24fcecdd87c115da98c19f2eb0643efc25
nouvelle <nouvellenuts@gmail.com>
2020-06-22 00:38:28 +0900

    Format: created_at

--- server/tweets.js
+++ server/tweets.js
@@ -3,17 +3,11 @@ const moment = require("moment");
 const customizeTweets = (datas) => {
   return datas.map(data => {
     let imgUrl = "";
-    if(data.entities.media && data.entities.media.length > 0){
-      // console.log("media :", data.entities.media[0].media_url);
-      imgUrl = data.entities.media[0].media_url;
-    // } else if(data.entities.urls && data.entities.urls.length > 0){
-    //   console.log("urls :", data.entities);
-    //   imgUrl = data.entities.urls[0].url;
-    }
+    if(data.entities.media && data.entities.media.length > 0) imgUrl = data.entities.media[0].media_url;
+    
     return {
-      // created_at: moment(data.created_at).format("YYYY/MM/DD ddd HH:mm"),
       id: data.id,
-      created_at: data.created_at,
+      created_at: moment(new Date(data.created_at)).format("YYYY/MM/DD ddd HH:mm"),
       text: data.text,
       imgUrl: imgUrl
     }
