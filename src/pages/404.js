30e068d6defb4c1b57c35373d4241ca43fdb908a
nouvelle <nouvellenuts@gmail.com>
2020-06-21 23:09:25 +0900

    Add: 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "../components/header";
+
+const Home = () => (
+  <div>
+    <Header title="Not Found"/>
+    <div>Not Found :(</div>
+  </div>
+);
+
+export default Home;
