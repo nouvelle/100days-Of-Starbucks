786d092236e5962f782b431312353572d9b2f376
nouvelle <nouvellenuts@gmail.com>
2020-06-30 22:29:40 +0900

    Handle scroll

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { useSelector } from "react-redux";
+import { css } from "emotion";
 import './Header.scss';
 
 function Header() {
+  const yScroll = useSelector(state => state.yScroll);
+
   return (
     <>
       <header className="home-box">
-        <div id="logo">#100DaysOfStarbucks</div>
+        <div id="logo" className={css`
+          transform: translate(0px, ${yScroll /2}%)
+        `}>#100DaysOfStarbucks</div>
       </header>
     </>
   );
