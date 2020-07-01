8b40f8b6766b1f803eb6646ac382c71dc0ec02b4
nouvelle <nouvellenuts@gmail.com>
2020-07-02 00:44:49 +0900

    bug fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,13 @@ import './Header.scss';
 
 function Header() {
   const yScroll = useSelector(state => state.yScroll);
-
+  
   return (
     <>
       <header className="home-box">
-        <div id="logo" styles={`
-          transform: translate(0px, ${yScroll /2}%)
-        `}>#100DaysOfStarbucks</div>
+        <div id="logo" style={{
+          transform: `translate(0px, ${yScroll / 2}%)`
+        }}>#100DaysOfStarbucks</div>
       </header>
     </>
   );
