5657136790fe668ce64b8da5ed7f03a8e0aad5f5
nouvelle <nouvellenuts@gmail.com>
2020-07-02 00:14:03 +0900

    Change style

--- src/components/Section/Garally/Figure.jsx
+++ src/components/Section/Garally/Figure.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import figureData from "../../../data/figures.json";
 
 function Figure() {
-
+  const figures = figureData.figures;
+  console.log(figureData.figures);
 
   return (
     <>
@@ -18,8 +20,6 @@ function Figure() {
           <img src="./img/tate03.jpg" alt="" />
           <figcaption>drinking coffee Santa <strong>2019/12/23</strong></figcaption>
         </figure>
-      </div>
-      <div className="row img-row no-sp">
         <figure className="columns four">
           <img src="./img/tate03.jpg" alt="" />
           <figcaption>drinking coffee Santa <strong>2019/12/23</strong></figcaption>
@@ -32,8 +32,6 @@ function Figure() {
           <img src="./img/tate02.jpg" alt="" />
           <figcaption>Frappuccino! <strong>2020/02/12</strong></figcaption>
         </figure>
-      </div>
-      <div className="row img-row no-sp">
         <figure className="columns four">
           <img src="./img/tate02.jpg" alt="" />
           <figcaption>Frappuccino! <strong>2020/02/12</strong></figcaption>
