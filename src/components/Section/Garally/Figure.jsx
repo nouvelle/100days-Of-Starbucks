585c0283454dae1779113680847a3a7a2b209204
nouvelle <nouvellenuts@gmail.com>
2020-07-02 11:45:52 +0900

    Display data from json

--- src/components/Section/Garally/Figure.jsx
+++ src/components/Section/Garally/Figure.jsx
@@ -3,47 +3,22 @@ import figureData from "../../../data/figures.json";
 
 function Figure() {
   const figures = figureData.figures;
-  console.log(figureData.figures);
+
+  function figureList() {
+    if(figures.length > 0) {
+      return  figures.map(obj => 
+        <figure key={obj.id} className="columns four">
+          <img src={obj.img} alt="" />
+          <figcaption>{obj.captipn} <strong>{obj.date}</strong></figcaption>
+        </figure>
+      );
+    }
+  }
 
   return (
     <>
       <div className="row img-row">
-        <figure className="columns four">
-          <img src="./img/tate01.jpg" alt="" />
-          <figcaption>1st Store in Japan. <strong>2019/08/02</strong></figcaption>
-        </figure>
-        <figure className="columns four">
-          <img src="./img/tate02.jpg" alt="" />
-          <figcaption>Frappuccino! <strong>2020/02/12</strong></figcaption>
-        </figure>
-        <figure className="columns four">
-          <img src="./img/tate03.jpg" alt="" />
-          <figcaption>drinking coffee Santa <strong>2019/12/23</strong></figcaption>
-        </figure>
-        <figure className="columns four">
-          <img src="./img/tate03.jpg" alt="" />
-          <figcaption>drinking coffee Santa <strong>2019/12/23</strong></figcaption>
-        </figure>
-        <figure className="columns four">
-          <img src="./img/tate01.jpg" alt="" />
-          <figcaption>1st Store in Japan. <strong>2019/08/02</strong></figcaption>
-        </figure>
-        <figure className="columns four">
-          <img src="./img/tate02.jpg" alt="" />
-          <figcaption>Frappuccino! <strong>2020/02/12</strong></figcaption>
-        </figure>
-        <figure className="columns four">
-          <img src="./img/tate02.jpg" alt="" />
-          <figcaption>Frappuccino! <strong>2020/02/12</strong></figcaption>
-        </figure>
-        <figure className="columns four">
-          <img src="./img/tate03.jpg" alt="" />
-          <figcaption>drinking coffee Santa <strong>2019/12/23</strong></figcaption>
-        </figure>
-        <figure className="columns four">
-          <img src="./img/tate01.jpg" alt="" />
-          <figcaption>1st Store in Japan. <strong>2019/08/02</strong></figcaption>
-        </figure>
+        {figureList()}
       </div>
     </>
   );
