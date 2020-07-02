3e8683cc494808fe9891ec3637b72c7e35622777
nouvelle <nouvellenuts@gmail.com>
2020-07-02 12:39:10 +0900

    Display data from json - Posts

--- src/components/Section/Posts/Posts.jsx
+++ src/components/Section/Posts/Posts.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import { useSelector } from "react-redux";
+import postsData from "../../../data/posts.json";
 import './Posts.scss';
 
 function Posts() {
-  const yScroll = useSelector(state => state.yScroll);
-  const post1 = document.getElementById("post-1");
-  const post3 = document.getElementById("post-3");
+  const post = document.getElementsByClassName("post");
+  const posts = postsData.posts;
 
   function animation(el) {
-    if(yScroll > el.offsetTop - window.innerHeight) {
-      const offset = (Math.min(0, yScroll - el.offsetTop + window.innerHeight - 950)).toFixed();
-  
-      post1.style.transform = `translate(${offset}px, ${Math.abs(offset * 0.2)}px)`;
-      post3.style.transform = `translate(${Math.abs(offset)}px, ${Math.abs(offset * 0.2)}px)`;
+    if(window.innerHeight > el.getBoundingClientRect().top + 50){
+      for(let i = 0; i < post.length; i++){
+        setTimeout(() => {
+          post[i].classList.add("is-showing");
+        },  (1000 * (Math.exp(i * 0.14))) - 700);
+      }
+    }
+  }
+  function PostList() {
+    if(posts.length > 0) {
+      return  posts.map(obj => 
+        <div key={obj.id} className="post columns four">
+          <h5>{obj.title}</h5>
+          <img src={obj.img} alt="" />
+          <p>{obj.comment}</p>
+          <p><a className="btn" href={obj.link}>Read More</a></p>
+        </div>
+      );
     }
   }
   
@@ -29,33 +41,7 @@ function Posts() {
         if(!el) return;
         animation(el);
       }}>
-      <div id="post-1" className="post columns four">
-        <h5>Tweet title</h5>
-        <img src="./img/post68.jpg" alt="" />
-        <p>
-          [68/100] #100DaysOfStarbucks <br />
-          ゴロッと イチゴ フラペチーノ🍓<br />
-        </p>
-        <p><a className="btn" href="https://twitter.com/e_chai/status/1275646396734025728">Read More</a></p>
-      </div>
-      <div id="post-2" className="post columns four">
-        <h5>Tweet title</h5>
-        <img src="./img/post67.jpg" alt="" />
-        <p>
-          [67/100] #100DaysOfStarbucks <br />
-          何ヶ月ぶりだろう、イートインスタバ🥺❤️
-        </p>
-        <p><a className="btn" href="https://twitter.com/e_chai/status/1271041858882252802">Read More</a></p>
-      </div>
-      <div id="post-3" className="post columns four">
-        <h5>Tweet title</h5>
-        <img src="./img/post66.jpg" alt="" />
-        <p>
-          [66/100] #100DaysOfStarbucks <br />
-          スタバの と New Mac の初コラボ☀️
-        </p>
-        <p><a className="btn" href="https://twitter.com/e_chai/status/1270525690396291075">Read More</a></p>
-      </div>
+      {PostList()}
     </div>
     </>
   );
