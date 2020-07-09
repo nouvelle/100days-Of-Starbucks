68222efdd0225e58c0a3989058a5d6c1ca9e9462
nouvelle <nouvellenuts@gmail.com>
2020-07-09 21:40:01 +0900

    Add moment-timezone

--- server/tweets.js
+++ server/tweets.js
@@ -1,4 +1,4 @@
-const moment = require("moment");
+const moment = require("moment-timezone");
 
 const getSpecificHashTweets = (specificHashTweetArr) => {
   return specificHashTweetArr.filter(allObj => allObj.entities.hashtags.length > 0)
@@ -11,10 +11,10 @@ const getSpecificHashTweets = (specificHashTweetArr) => {
       const link = data.full_text.slice(-23);
       let imgUrl = "";
       if(data.entities.media && data.entities.media.length > 0) imgUrl = data.entities.media[0].media_url;
-
+      
       return {
         id: data.id,
-        created_at: moment(new Date(data.created_at)).format("YYYY/MM/DD ddd HH:mm"),
+        created_at: moment(new Date(data.created_at)).tz("Asia/Tokyo").format("YYYY/MM/DD ddd HH:mm"),
         img: imgUrl,
         link: link,
         comment: text,
